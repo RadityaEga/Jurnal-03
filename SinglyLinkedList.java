efa1e632bd39d670847b8ce2e5a15b3ffe670b48
Raditya Ega <89286716+RadityaEga@users.noreply.github.com>
2022-03-19 23:03:55 +0700

    Create SinglyLinkedList.java

---
+++ SinglyLinkedList.java
@@ -0,0 +1,108 @@
+import java.util.List;
+import java.util.NoSuchElementException;
+
+    public class SinglyLinkedList<E> {
+        private ListNode<E> firstNode;
+        private ListNode<E> lastNode;
+        private String name;
+
+        public SinglyLinkedList() {
+            this ("Linked List");
+        }
+
+        public SinglyLinkedList(String listName) {
+            name = listName;
+            firstNode = lastNode = null;
+        }
+
+        public void insertAtFront (E insertItem) {
+            ListNode<E> newNode = new ListNode<E>(insertItem);
+            if (isEmpty()) {
+                firstNode = lastNode = new ListNode<E>(insertItem);
+            }
+
+            else {
+                newNode.next = firstNode;
+                firstNode = newNode;
+            }
+        }
+
+        public void insertAtBack (E insertItem) {
+            ListNode<E> newNode = new ListNode<E>(insertItem);
+            if (isEmpty()) {
+                firstNode = lastNode = new ListNode<E>(insertItem);
+            }
+
+            else {
+                lastNode.next = newNode;
+                lastNode = newNode;
+            }
+        }
+
+        public E removeFromFront() throws NoSuchElementException {
+            if (isEmpty()) {
+                throw new NoSuchElementException(name + "kosong");
+            }
+
+            E removedItem = firstNode.data;
+            if (firstNode == lastNode) {
+                firstNode = lastNode = null;
+            }
+
+            else {
+                ListNode<E> current = firstNode;
+
+                while  (current.next != firstNode ) {
+                    current = current.next;
+                }
+                firstNode = current;
+                current.next = null;
+            }
+
+            return removedItem;
+        }
+
+        public E removeFromBack() throws NoSuchElementException {
+            if (isEmpty()) {
+                throw new NoSuchElementException(name + "kosong");
+            }
+
+            E removedItem = lastNode.data;
+            if (firstNode == lastNode) {
+                firstNode = lastNode = null;
+            }
+
+            else {
+                ListNode<E> current = firstNode;
+
+                while  (current.next != lastNode ) {
+                    current = current.next;
+                }
+                lastNode = current;
+                current.next = null;
+            }
+
+            return removedItem;
+        }
+
+        private boolean isEmpty() {
+            return firstNode == null;
+        }
+
+        public void print() {
+            if (isEmpty()) {
+                System.out.printf("%s kosong %n", name);
+                return;
+            }
+
+            System.out.printf("Berikut adalah %s : %n", name);
+            ListNode<E> current = firstNode;
+
+            while (current != null) {
+                System.out.printf("%s ",current.data);
+                current = current.next;
+            }
+
+            System.out.println();
+        }
+    }
